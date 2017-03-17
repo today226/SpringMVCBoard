a3f408446f7ca909c36af3ab958891c563bf9feb
today226 <today226@DESKTOP-G5OCLI2>
2017-03-18 00:01:57 +0900

    비즈니스 로직의 수행을 위한 메서드를 정의

---
+++ SpringMVCBoard/src/main/java/com/spring/board/service/BoardService.java
@@ -0,0 +1,10 @@
+package com.spring.board.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface BoardService {
+
+	List<Map<String, Object>> selectBoardList(Map<String, Object> map) throws Exception;
+
+}
