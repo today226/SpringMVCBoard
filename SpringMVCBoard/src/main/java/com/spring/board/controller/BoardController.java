0e24e5b38101b7924d95d86700705e8fa9435d97
today226 <today226@nate.com>
2017-03-21 00:59:24 +0900

    CustomMapArgumentResolver 테스트 컨트롤러 등록

--- SpringMVCBoard/src/main/java/com/spring/board/controller/BoardController.java
+++ SpringMVCBoard/src/main/java/com/spring/board/controller/BoardController.java
@@ -1,7 +1,9 @@
 package com.spring.board.controller;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.annotation.Resource;
 
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.spring.board.common.map.CommandMap;
 import com.spring.board.service.BoardService;
 
 
@@ -26,6 +29,21 @@ public class BoardController {
 		
 		List<Map<String, Object>> list = boardService.selectBoardList(commandMap);
 		mv.addObject("list", list);
+		return mv;
+	}
+	
+	@RequestMapping(value="/board/testMapArgumentResolver.com")
+	public ModelAndView testMapArgumentResolver(CommandMap commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("");
+		
+		if(commandMap.isEmpty() == false){
+			Iterator<Entry<String, Object>> iterator = commandMap.getMap().entrySet().iterator();
+			Entry<String, Object> entry = null;
+			while(iterator.hasNext()){
+				entry = iterator.next();
+				log.debug("key : " + entry.getKey() + ", value : " + entry.getValue());
+			}
+		}
 		return mv;
 	}
 }
